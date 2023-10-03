f5ded5e57f03f6b00a1944fa2bf33f19c188412c
xNECROMANCERx <hakeemhamza147@gmail.com>
2023-10-03 11:57:41 +0530

    After making some modifications, the program now functions correctly.

--- guess.py
+++ guess.py
@@ -3,12 +3,28 @@ import random
 import string
 nums=random.choice(list(range(0,101)))
 print(nums)
-a=int(input("Enter a number: \n"))
-if (nums-a ==0):
-    print("congrats")
-    exit(0)
-else:
-    print("not really")
-    exit(0)
+a=int(input("guess a number:\n"))
+def numm(a):
+    while (a!=nums):
+        a=int(input("Enter a number: \n"))
+        if (nums-a ==0):
+            print("congrats")
+            exit(0)
+        elif (nums-a<10 and nums-a>0):
+            print("A bit low")
+            continue
+        elif (nums-a>10 ):
+            print("Too low")
+            continue
+        elif (nums-a< -10):
+            print('Too high')
+            continue
+        elif (nums-a> -10 and nums-a <0):
+            print("A bit high")
+            continue
+        else:
+            print("not really")
+            exit(0)
+numm(a)
 
 
